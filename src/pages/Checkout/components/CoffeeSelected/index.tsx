c53f749ea16dc20484b3e13db3ce0ad4e092f7de
Matheus <matheusdeamorim20@gmail.com>
2023-05-03 12:05:32 -0300

    working on coffeeContext

--- src/pages/Checkout/components/CoffeeSelected/index.tsx
+++ src/pages/Checkout/components/CoffeeSelected/index.tsx
@@ -1,31 +1,41 @@
 import styles from './CoffeeSelected.module.css'
-import capuccino from '../../../../../public/capuccino.png'
 import { Trash } from '@phosphor-icons/react'
 
-export function CoffeeSelected() {
+interface CoffeeAtributes {
+    id: string
+    title: string
+    price: number
+    imgUrl: string 
+    amount?: number
+}
+
+export function CoffeeSelected({ imgUrl, title, price }: CoffeeAtributes) {
   return (
-    <div className={styles.coffeeSelected}>
-        <div className={styles.info}>
-            <img src={capuccino} width={64}/>
-            <div className={styles.details}>
-                <span> Expresso Tradicional </span>
-                <div className={styles.actions}>
-                    <div className={styles.counter}>
-                        <button className={styles.minus}>
-                            -
-                        </button>
-                        <span> 1 </span>
-                        <button className={styles.plus}>
-                            +
+    <>
+        <div className={styles.coffeeSelected}>
+            <div className={styles.info}>
+                <img src={imgUrl} width={64}/>
+                <div className={styles.details}>
+                    <span> {title} </span>
+                    <div className={styles.actions}>
+                        <div className={styles.counter}>
+                            <button className={styles.minus}>
+                                -
+                            </button>
+                            <span> 1 </span>
+                            <button className={styles.plus}>
+                                +
+                            </button>
+                        </div>
+                        <button className={styles.remove}>
+                            <Trash color='#8047F8'/> REMOVER
                         </button>
                     </div>
-                    <button className={styles.cart}>
-                        <Trash color='#8047F8'/> REMOVER
-                    </button>
                 </div>
             </div>
+            <span className={styles.price}> {price} </span>
         </div>
-        <span> R$ 9,90 </span>
-    </div>
+        <div className={styles.divider}></div>
+    </>
   )
 }
